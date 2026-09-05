0fdd93952104cae9509ca1cdfc9d53c814e9eb98
blair019 <you@example.com>
2026-09-06 05:47:57 +0800

    cors fix

--- app/main.py
+++ app/main.py
@@ -43,6 +43,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://frontend-tuntunan.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
